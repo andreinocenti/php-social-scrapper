091d9df8117f099188bb0274f746be669b3a2782
Andre Inocenti <andreinocenti@gmail.com>
2024-08-12 18:58:40 -0300

    feat: tests config

---
+++ tests/TestCase.php
@@ -0,0 +1,11 @@
+<?php
+
+// namespace Tests;
+namespace AndreInocenti\PhpSocialScrapper\Tests;
+
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+    //
+}
